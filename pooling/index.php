fd8d5b8cce4bc8da589628a642cd78ccd97d5c4e
nesverth <dheo.pratama@gmail.com>
2023-01-30 22:55:04 +0700

    Update index.php
    update url system back to classic without htaccess

--- pooling/index.php
+++ pooling/index.php
@@ -4,23 +4,26 @@ session_start();
 require( "config.php" );
 $media = isset($_GET["media"]) ? $_GET["media"] : "display";
 
+/*
+ * 
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-
 $arr_path = explode("/",$path);
-
 $action = strtolower($arr_path[(count($arr_path)-1)]);
+
 if(empty($action)) {
   $action =  $arr_path[(count($arr_path)-2)];
 }
 if($action == APP_DIR) {
   $action = "home";
 }
-//var_dump($arr_path);die();
-//$action = isset($_GET["action"]) ? strtolower($_GET["action"]) : "404";
+ * 
+ */
+
+$action = isset($_GET["action"]) ? strtolower($_GET["action"]) : "home";
 
 $api_list = Menu::getListApi();
 $in_api = array_search($action,$api_list,true);
-if($in_api == false) {
+if($in_api === false) {
   include "web.php";
 } else {
   include "api.php";
