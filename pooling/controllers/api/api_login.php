c5b8f18f983495a6010e93f53a66b1c5732f5fc4
Dheo <dheo.pratama@gmail.com>
2023-01-31 17:50:31 +0700

    fix minor bug

--- pooling/controllers/api/api_login.php
+++ pooling/controllers/api/api_login.php
@@ -1,6 +1,6 @@
 <?php 
 if($action == "api_login_mobile") {
-  $usrid = $_REQUEST["usrid"];
+  $usrid = trim(explode("|",$_REQUEST["usrid"])[0]);
   $class = new User();
   $login = $class->loginMobile($usrid);
   echo json_encode($login);
