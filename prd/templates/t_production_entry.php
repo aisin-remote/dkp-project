a1390aea6471d6b326dcf73c294dd1943d87d8ee
Dheo <Dheo@EGA-NB0002.lan>
2023-05-05 14:54:02 +0700

    Minor Fix

--- prd/templates/t_production_entry.php
+++ prd/templates/t_production_entry.php
@@ -18,17 +18,37 @@ and open the template in the editor.
     <div id="layoutSidenav_content">
       <main>
         <div class="container-fluid mt-4">
-          <ol class="breadcrumb mb-4 mt-4">
+          <ol class="breadcrumb mb-2 mt-4">
             <li class="breadcrumb-item"><?php echo $template["group"]; ?></li>
             <li class="breadcrumb-item active"><?php echo $template["menu"]; ?></li>
             <li class="breadcrumb-item active"><?php echo $template["submenu"]; ?></li>
           </ol>
+          <?php
+          if (isset($_GET["error"])) {
+            echo '<div class="alert alert-danger alert-dismissible fade show mt-1" role="alert">
+                      Error : ' . $_GET["error"] . '
+                      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                      </button>
+                    </div>';
+          }
+          ?>
 
+          <?php
+          if (isset($_GET["success"])) {
+            echo '<div class="alert alert-success alert-dismissible fade show mt-1" role="alert">
+                      Success : ' . $_GET["success"] . '
+                      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                      </button>
+                    </div>';
+          }
+          ?>
           <form method="get" action="?action=<?php echo $action; ?>">
             <input type="hidden" name="action" value="<?= $action ?>">
             <div class="row">
               <div class="col-12">
-                <div class="card mt-2">
+                <div class="card mt-0">
                   <div class="card-body">
                     <!-- Edit Here -->
 
