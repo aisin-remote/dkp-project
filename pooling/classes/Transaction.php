5243a80feed486c3584e10d4161db0addb01054f
Dheo <dheo.pratama@gmail.com>
2023-05-13 10:28:24 +0700

    Delivery checklist

---
+++ pooling/classes/Transaction.php
@@ -0,0 +1,132 @@
+<?php
+
+class Transaction
+{
+    public function getList($lddat_from = "*", $lddat_to = "*", $fil_cust = null)
+    {
+        $return = array();
+        $conn = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
+        $sql = "SELECT a.*, a.ldnum, a.pdsno, a.cycle1, to_char(a.lddat, 'DD-MM-YYYY') as date_only, "
+            . " to_char(a.lddat, 'HH24:MI') as time_only, a.stats, "
+            . " (select name1 from m_io_lfa1 where lifnr = a.lifnr) as customer "
+            . " FROM t_io_ldlist_h a  "
+            . " WHERE a.dstat IS NULL ";
+
+        if ($lddat_from !== "*" && $lddat_to !== "*") {
+            $sql .= " AND TO_CHAR(a.lddat, 'YYYYMMDD') between '$lddat_from' AND '$lddat_to' ";
+        }
+        if (!empty($fil_cust)) {
+            $sql .= " AND a.lifnr = '$fil_cust' ";
+        }
+
+        $sql .= " ORDER by a.ldnum ASC ";
+
+        $stmt = $conn->prepare($sql);
+        if ($stmt->execute()) {
+            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                if ($row["stats"] == "N") {
+                    $row["stats"] = "UNCOMPLETED";
+                } elseif ($row["stats"] == "C") {
+                    $row["stats"] = "COMPLETED";
+                }
+
+                if ($row["dstat"] == null || $row["dstat"] == "") {
+                    $row["dstat"] = "NOT DELIVERED";
+                } elseif ($row["dstat"] == "D") {
+                    $row["dstat"] = "DELIVERED";
+                }
+                $return[] = $row;
+            }
+        }
+        $stmt = null;
+        $conn = null;
+        return $return;
+    }
+
+    public function updateStatus($extract_id)
+    {
+        $return = array();
+        $conn = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
+        $sql = "UPDATE public.t_io_ldlist_h SET dstat = 'D' "
+            . "WHERE ldnum IN('$extract_id')";
+
+        // echo $sql;
+        // exit;
+        $stmt = $conn->prepare($sql);
+        if ($stmt->execute()) {
+            $return["status"] = true;
+        } else {
+            $error = $stmt->errorInfo();
+            $return["status"] = false;
+            $return["message"] = trim(str_replace("\n", " ", $error[2]));
+            error_log($error[2]);
+        }
+        $stmt = null;
+        $conn = null;
+        return $return;
+    }
+    public function updateStatus2($extract_id)
+    {
+        $return = array();
+        $conn = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
+        $sql = "UPDATE public.t_io_ldlist_i SET dstat = 'D', dstat_dats = CURRENT_TIMESTAMP "
+            . "WHERE ldnum IN('$extract_id')";
+
+        // echo $sql;
+        // exit;
+        $stmt = $conn->prepare($sql);
+        if ($stmt->execute()) {
+            $return["status"] = true;
+        } else {
+            $error = $stmt->errorInfo();
+            $return["status"] = false;
+            $return["message"] = trim(str_replace("\n", " ", $error[2]));
+            error_log($error[2]);
+        }
+        $stmt = null;
+        $conn = null;
+        return $return;
+    }
+    public function updateStatus3($extract_id)
+    {
+        $return = array();
+        $conn = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
+        $sql = "UPDATE public.t_io_ldlist_dtl SET dstat = 'D', dstat_dats = CURRENT_TIMESTAMP "
+            . "WHERE ldnum IN('$extract_id')";
+
+        // echo $sql;
+        // exit;
+        $stmt = $conn->prepare($sql);
+        if ($stmt->execute()) {
+            $return["status"] = true;
+        } else {
+            $error = $stmt->errorInfo();
+            $return["status"] = false;
+            $return["message"] = trim(str_replace("\n", " ", $error[2]));
+            error_log($error[2]);
+        }
+        $stmt = null;
+        $conn = null;
+        return $return;
+    }
+
+    public function getCustomer()
+    {
+        $return = array();
+        $conn = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
+        $sql = "SELECT lifnr, name1 FROM m_io_lfa1 ";
+
+        $sql .= " ORDER by lifnr ASC ";
+        $stmt = $conn->prepare($sql);
+        if ($stmt->execute()) {
+            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                $return[] = $row;
+            }
+        }
+        $stmt = null;
+        $conn = null;
+        return $return;
+    }
+}
+
+?>
---
+++ pooling/templates/t_loading_list.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+
+<head>
+  <?php include "common/t_css.php"; ?>
+  <link href="vendors/ega/css/styles.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+  <?php include "common/t_nav_top.php"; ?>
+  <div id="layoutSidenav">
+    <?php include "common/t_nav_left.php"; ?>
+    <div id="layoutSidenav_content">
+      <main>
+        <div class="container-fluid">
+          <ol class="breadcrumb mb-4 mt-4">
+            <li class="breadcrumb-item">
+              <?php echo $template["group"]; ?>
+            </li>
+            <li class="breadcrumb-item active">
+              <?php echo $template["menu"]; ?>
+            </li>
+          </ol>
+          <?php
+          if (isset($_GET["error"])) {
+            echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+                      Error : ' . $_GET["error"] . '
+                      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                      </button>
+                    </div>';
+          }
+          ?>
+
+          <?php
+          if (isset($_GET["success"])) {
+            echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+                      Success : ' . $_GET["success"] . '
+                      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                      </button>
+                    </div>';
+          }
+          ?>
+          <div class="row">
+            <div class="col-12">
+              <div class="card mt-2">
+                <div class="card-body">
+                  <!-- Edit Here -->
+                  <div class="container-fluid">
+                    <div class="table-responsive text-nowrap">
+                      <form action="?action=<?php echo $action; ?>" method="POST">
+                        <table class="table table-sm" id="data-table-x">
+                          <thead>
+                            <tr>
+                              <th class="align-middle pl-2"><input id='checkAll' type='checkbox'
+                                  style='height: 18px; width: 18px;' /></th>
+                              <th class=''>Loading List Number</th>
+                              <th class=''>PDS Number</th>
+                              <th class=''>Customer</th>
+                              <th class=''>P. Code</th>
+                              <th class=''>Receive Area</th>
+                              <th class=''>Cycle</th>
+                              <th class=''>Delivery Date</th>
+                              <th class=''>Delivery Time</th>
+                              <th class=''>Pulling Status</th>
+                              <th class=''>Delivery Status</th>
+                              <!-- <th class='text-center'>Device Antenna</th> -->
+                            </tr>
+                          </thead>
+                          <tbody>
+                            <?php
+                            if (!empty($list)) {
+                              foreach ($list as $row) {
+                                echo
+                                  "<tr>"
+                                  . "<td class='align-middle chkbox pl-2'><input name='chk_id[]' type='checkbox' value='" . $row["ldnum"] . "' style='height: 18px; width: 18px;' /></td>"
+                                  . "<td class='align-middle'>" . $row["ldnum"] . "</td>"
+                                  . "<td class='align-middle'>" . $row["pdsno"] . "</td>"
+                                  . "<td class='align-middle'>" . $row["customer"] . "</td>"
+                                  . "<td class='align-middle'>" . $row["werks"] . "</td>"
+                                  . "<td class='align-middle'>" . $row["rcvar"] . "</td>"
+                                  . "<td class='align-middle'>" . $row["cycle1"] . "</td>"
+                                  . "<td class='align-middle'>" . $row["date_only"] . "</td>"
+                                  . "<td class='align-middle'>" . $row["time_only"] . "</td>"
+                                  . "<td class='align-middle'>" . $row["stats"] . "</td>"
+                                  . "<td class='align-middle'>" . $row["dstat"] . "</td>"
+                                  . "</tr>";
+                              }
+                            }
+                            ?>
+                          </tbody>
+                        </table>
+                        <button name="chg_status" type="submit" class="btn btn-template btn-sm mb-3">Delivered</button>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+      <?php include 'common/t_footer.php'; ?>
+    </div>
+  </div>
+  <div class="modal fade" id="modal_filter" data-backdrop="static" data-keyboard="false" tabindex="-1"
+    aria-labelledby="modal_filter_label" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <form method="get" action="#">
+          <input type="hidden" name="action" value="<?= $action; ?>">
+          <div class="modal-header">
+            <h5 class="modal-title" id="modal_filter_label"><span class="material-icons">filter_alt</span> Filter</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body pb-1">
+            <div class="row">
+              <div class="col-2"><label class="col-form-label">Date</label></div>
+              <div class="col"><input type="text" name="date_from" class="form-control datepicker"
+                  value="<?php echo $lddat_from; ?>"></div>
+              <label class="col-form-label px-3">to</label>
+              <div class="col"><input type="text" name="date_to" class="form-control datepicker"
+                  value="<?php echo $lddat_to; ?>"></div>
+            </div>
+          </div>
+          <div class="modal-body pt-1">
+            <div class="row">
+              <div class="col-2"><label class="col-form-label">Customer</label></div>
+              <div class="col"><select name="customer" id="customer" class="form-control modalSelect01">
+                  <option value="">Pilih Customer</option>
+                  <?php
+                  foreach ($customer as $cust) {
+                    ?>
+                    <option value="<?php echo $cust["lifnr"]; ?>" <?php
+                       if ($cust['name1'] == $_GET["customer"]) {
+                         echo "selected";
+                       }
+                       ?>><?php echo $cust["name1"]; ?></option>
+                    <?php
+                  }
+                  ?>
+                </select></div>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-outline-dark" data-dismiss="modal">Cancel</button>
+            <button type="submit" class="btn btn-primary" name="filter" id="filter" value="filter">Filter</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+
+  <?php include 'common/t_js.php'; ?>
+  <script src="vendors/ega/js/scripts.js?time=<?php echo date("Ymdhis"); ?>" type="text/javascript"></script>
+  <script>
+    $(document).ready(function () {
+    });
+
+    $(".datepicker").flatpickr({
+      altInput: true,
+      altFormat: "d-m-Y",
+      dateFormat: "Ymd"
+    });
+    $("#data-table-x").DataTable({
+      stateSave: true,
+      order: [
+        [0, 'asc']
+      ],
+      dom: "<'row'<'col-sm-12 col-md-6'B><'col-sm-12 col-md-2'l><'col-sm-12 col-md-4'f>>" +
+        "<'row'<'col-sm-12'tr>>" +
+        "<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7'p>>",
+      buttons: [{
+        className: 'btn btn-primary btn-sm',
+        text: '<i class="material-icons">filter_alt</i> Filter',
+        action: function () {
+          $('#modal_filter').modal("show");
+
+        }
+      }
+      ]
+    });
+
+    $('.modalSelect01').select2({
+      dropdownParent: $('#modal_filter'),
+      theme: 'bootstrap4',
+      width: '100%'
+    });
+
+    $('td').each(function () {
+      if ($(this).html() == 'COMPLETED') {
+        $(this).css('color', 'green');
+      } else if ($(this).html() == 'UNCOMPLETED') {
+        $(this).css('color', 'red');
+      } else if ($(this).html() == 'NOT DELIVERED') {
+        $(this).css('color', 'red');
+      } else if ($(this).html() == 'DELIVERED') {
+        $(this).css('color', 'greeb');
+      }
+    });
+
+    var table = $('#data-table-x').DataTable();
+    table.on('draw.dt', function () {
+      $('td').each(function () {
+        if ($(this).html() == 'COMPLETED') {
+          $(this).css('color', 'green');
+        } else if ($(this).html() == 'UNCOMPLETED') {
+          $(this).css('color', 'red');
+        } else if ($(this).html() == 'NOT DELIVERED') {
+          $(this).css('color', 'red');
+        } else if ($(this).html() == 'DELIVERED') {
+          $(this).css('color', 'green');
+        }
+      });
+    });
+
+    $("#checkAll").change(function () {
+      $("input[type='checkbox']").prop("checked", this.checked);
+    });
+  </script>
+</body>
+
+</html>
