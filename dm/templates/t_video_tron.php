95ef93f45e62668a95c72037519c6ab854eb8ae3
Dheo <Dheo@EGA-NB0002.lan>
2023-09-14 18:32:55 +0700

    Update tampilan videotron

--- dm/templates/t_video_tron.php
+++ dm/templates/t_video_tron.php
@@ -24,6 +24,10 @@ and open the template in the editor.
         font-weight: 900;
         line-height: 1.3 !important;
       }
+      
+      .font-agak-kecil {
+        font-size: 72px !important;
+      }
     </style>
   </head>
 
@@ -34,66 +38,48 @@ and open the template in the editor.
           <div class="px-1 bg-white h-100" id="fs">
             <div id="carouselExampleIndicators" class="carousel slide h-100" data-ride="carousel">
               <div class="carousel-inner h-100">
-                <div class="carousel-item active h-100">
+                <div class="carousel-item active h-100" data-interval="1000">
                   <div class="d-flex justify-content-center align-items-center my-auto h-100">
                     <h1 class='disp-1'>Dies Map</h1>
                   </div>
                 </div>
                 <?php
-                //generate lokasi parkir
-                foreach ($list_zona as $list) {
-                  if ($list["zona_type"] == "P") {
-                ?>
-                <div class="h-100 carousel-item <?=$list["bg"]?>">
-                  <p><?=$list["desc"]?></p>
-                  <hr>
-                  <div class="container-fluid">
-                    <div id="zona_<?=$list["zona_id"]?>" class="data-zona row">
-
-                    </div>
-                  </div>
-                </div>
-                <?php
-                  }
-                }
-                ?>
-                <?php
-                //generate lokasi produksi
-                foreach ($list_zona as $list) {
-                  if ($list["zona_type"] == "L") {
-                ?>
-                <div class="h-100 carousel-item <?=$list["bg"]?>">
-                  <p><?=$list["desc"]?></p>
-                  <hr>
-                  <div class="container-fluid">
-                    <div id="zona_<?=$list["zona_id"]?>" class="data-zona row">
-
-                    </div>
-                  </div>
-                </div>
-                <?php
-                  }
-                }
-                ?>
-                <?php
                 //generate lokasi Maintenance
+                $mte_zone = [];
+                $zone_count = 0;
                 foreach ($list_zona as $list) {
                   if ($list["zona_type"] == "M") {
-                ?>
-                <div class="h-100 carousel-item <?=$list["bg"]?>">
-                  <p><?=$list["desc"]?></p>
-                  <hr>
-                  <div class="container-fluid">
-                    <div id="zona_<?=$list["zona_id"]?>" class="data-zona row">
-
-                    </div>
-                  </div>
-                </div>
-                <?php
+                    $mte_zone[$zone_count]["bg"] = $list["bg"];
+                    $mte_zone[$zone_count]["zona_id"] = $list["zona_id"];
+                    $mte_zone[$zone_count]["desc"] = $list["desc"];
+                    $zone_count++;
                   }
                 }
+                $zona_per_carousel = 4;
+                $sudah_tampil = 0;
+                for($i = 0;$i<$zone_count;$i++) {
+                  if($sudah_tampil == $zona_per_carousel - 1) {
+                    $sudah_tampil = 0;
+                    //reset carousel
+                    //awal carousel baru
+                    echo '<div class="col-6"><div class="container-fluid"><div class="row"><div class="col-12 bg-primary text-white text-truncate">'.$mte_zone[$i]["desc"].'</div><div class="col-12 bg-light data-zona px-0" id="zona_'.$mte_zone[$i]["zona_id"].'"><div class="border text-center">-</div></div></div></div></div>';
+                    echo '</div></div>';
+                  } else {
+                    if($sudah_tampil == 0) {
+                      //awal carousel
+                      echo '<div class="carousel-item" data-interval="2000">';
+                      echo '<div class="row">';
+                      echo '<div class="col-6"><div class="container-fluid"><div class="row"><div class="col-12 bg-primary text-white text-truncate">'.$mte_zone[$i]["desc"].'</div><div class="col-12 bg-light data-zona px-0" id="zona_'.$mte_zone[$i]["zona_id"].'"><div class="border text-center">-</div></div></div></div></div>';
+                    } else {
+                      // isi data
+                      echo '<div class="col-6"><div class="container-fluid"><div class="row"><div class="col-12 bg-primary text-white text-truncate">'.$mte_zone[$i]["desc"].'</div><div class="col-12 bg-light data-zona px-0" id="zona_'.$mte_zone[$i]["zona_id"].'"><div class="border text-center">-</div></div></div></div></div>';
+                    }
+                    $sudah_tampil++;
+                  }
+                  
+                }
                 ?>
-                <div class="h-100 carousel-item h-100">
+                <div class="h-100 carousel-item h-100" data-interval="1000">
                   <div class="d-flex justify-content-center align-items-center my-auto h-100">
                     <h1 class='disp-1'>Dies Status</h1>
                   </div>
@@ -110,7 +96,7 @@ and open the template in the editor.
                 for($i = 0; $i < $jumlah_carousel; $i++) {
                   $y = 0;
                 ?>
-                <div class="carousel-item">
+                <div class="carousel-item" data-interval="2000">
                   <div class="row mx-1">
                   <?php
                   while($x < $jumlah_data) {
@@ -118,16 +104,17 @@ and open the template in the editor.
                     if($y == 6) {
                       break;
                     }
+                    
                     echo "<div class='col-6 p-1'>
                             <div id='bg_colour_".$data_dies[$x]["dies_id"]."' class='container-fluid border text-dark ".$data_dies[$x]["bg_color"]."' style='background-color: ".$data_dies[$x]["colour"].";'>
                               <div class='row'>
-                                <div class='col-8'>
+                                <div class='col-8 px-1'>
                                   <p class='text-left mb-0'>".$data_dies[$x]["group_id"]." ".$data_dies[$x]["model_id"]."</p>
                                   <p class='text-left mb-0'>".$data_dies[$x]["dies_no"]."</p>
                                 </div>
-                                <div class='col-4'>
-                                  <p class='text-right mb-0'><span id='stkrun_".$data_dies[$x]["dies_id"]."'>".$data_dies[$x]["stkrun"]."</span></p>
-                                  <p class='text-right mb-0'><span id='stk6k_".$data_dies[$x]["dies_id"]."'>".$data_dies[$x]["stk6k"]."</span></p>
+                                <div class='col-4 px-1'>
+                                  <p class='text-right mb-0'><span class='font-agak-kecil' id='stkrun_".$data_dies[$x]["dies_id"]."'>".$data_dies[$x]["stkrun"]."</span></p>
+                                  <p class='text-right mb-0'><span class='font-agak-kecil' id='stk6k_".$data_dies[$x]["dies_id"]."'>".$data_dies[$x]["stk6k"]."</span></p>
                                 </div>
                               </div>
                             </div>
@@ -155,12 +142,13 @@ and open the template in the editor.
       setInterval(updateDashboardStatusDies, 5000);
       $(document).ready(function () {
         // closeFullscreen();
+        updateDashboardStatusDies();
       });
       
       function updateDashboardStatusDies() {
         $.getJSON("?action=api_dashboard_dm", {}, function (data) {
           var data_dies = data.data_dies;
-          $(".data-zona").html("");
+          $(".data-zona").html("<div class='border text-center'>-</div>");
           $.each(data_dies, function(row, value){
             $("#bg_colour_"+value.dies_id).removeClass();
             $("#bg_colour_"+value.dies_id).addClass("container-fluid border text-dark");
@@ -171,7 +159,7 @@ and open the template in the editor.
             $("#stkrun_"+value.dies_id).html(value.stkrun);
             $("#stk6k_"+value.dies_id).html(value.stk6k);
             
-            $("#zona_"+value.zona_id).append("<div class='col-6 text-dark p-1' style='background-color:"+value.colour+";'>"+value.group_id+" " + value.model_id + " " + value.dies_no + "</div>");
+            $("#zona_"+value.zona_id).html("<div class='border text-center' style='background-color:"+value.colour+";'>"+value.group_id+" " + value.model_id + " " + value.dies_no + "</div>");
           });
         });
       }
